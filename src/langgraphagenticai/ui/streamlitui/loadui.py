964b89315cdbcd48a102b53f4a01f1597ed29111
karan6100 <karanbari1996@gmail.com>
2025-03-09 17:47:32 +0530

    refreshed loadui

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -13,13 +13,10 @@ from src.langgraphagenticai.ui.uiconfigfile import Config
 
 class LoadStreamlitUI:
     def __init__(self):
-        self.config = Config()  # Loads configurations from uiconfigfile.py
+        self.config =  Config() # config
         self.user_controls = {}
 
-
-
     def initialize_session(self):
-        """Required for different usecases """
         return {
         "current_step": "requirements",
         "requirements": "",
@@ -29,54 +26,42 @@ class LoadStreamlitUI:
         "review_feedback": "",
         "decision": None
     }
-
-    # def render_requirements(self):
-    #     st.markdown("## Requirements Submission")
-    #     st.session_state.state["requirements"] = st.text_area(
-    #         "Enter your requirements:",
-    #         height=200,
-    #         key= "req_input"
-    #     )
-    #     if st.button("Submit Requirements", key = "submit_req"):
-    #         st.session_state.state["current_step"] = "generate_user_stories"
-    #         st.session_state.state.IsSDLC = True
-
+  
 
 
     def load_streamlit_ui(self):
-        """Basic UI functions which should be visible when page is loaded """
         st.set_page_config(page_title= "🤖 " + self.config.get_page_title(), layout="wide")
         st.header("🤖 " + self.config.get_page_title())
         st.session_state.timeframe = ''
         st.session_state.IsFetchButtonClicked = False
         st.session_state.IsSDLC = False
-       
+        
+        
 
         with st.sidebar:
-            # Getting options from config
+            # Get options from config
             llm_options = self.config.get_llm_options()
             usecase_options = self.config.get_usecase_options()
 
-            # LLM selection 
-            self.user_controls["selected_llm"] = st.selectbox("Select LLM", llm_options) # Creating Fieldbox
+            # LLM selection
+            self.user_controls["selected_llm"] = st.selectbox("Select LLM", llm_options)
 
-            if self.user_controls["selected_llm"] == "Groq":
-                # Model selections for Groq(refer uiconfigfile.ini & uiconfigfile.py)
+            if self.user_controls["selected_llm"] == 'Groq':
+                # Model selection
                 model_options = self.config.get_groq_model_options()
                 self.user_controls["selected_groq_model"] = st.selectbox("Select Model", model_options)
-                
-                # API input key (saving as session state)
-                self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"] = st.text_input("API Key", type="password")
-
-                # Validate API Key
+                # API key input
+                self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"] = st.text_input("API Key",
+                                                                                                      type="password")
+                # Validate API key
                 if not self.user_controls["GROQ_API_KEY"]:
                     st.warning("⚠️ Please enter your GROQ API key to proceed. Don't have? refer : https://console.groq.com/keys ")
+                   
             
             # Use case selection
             self.user_controls["selected_usecase"] = st.selectbox("Select Usecases", usecase_options)
 
-            if self.user_controls["selected_usecase"] == "Chatbot with Tools":
-
+            if self.user_controls["selected_usecase"] =="Chatbot with Tool":
                 # API key input
                 os.environ["TAVILY_API_KEY"] = self.user_controls["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"] = st.text_input("TAVILY API KEY",
                                                                                                       type="password")
@@ -84,12 +69,9 @@ class LoadStreamlitUI:
                 if not self.user_controls["TAVILY_API_KEY"]:
                     st.warning("⚠️ Please enter your TAVILY_API_KEY key to proceed. Don't have? refer : https://app.tavily.com/home")
             
-
-
-            # Initializing the session
             if "state" not in st.session_state:
                 st.session_state.state = self.initialize_session()
-            # self.render_requirements() 
-
-
+            
+            
+        
         return self.user_controls
